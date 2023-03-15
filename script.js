7b284febe61018ba8b41ea40e4707b5565aee112
unknown <venums46@gmail.com>
2023-03-15 16:16:46 +0600

    StrJs

---
+++ script.js
@@ -0,0 +1,75 @@
+// 1 task
+// function len(str) {
+//     return str.length
+// }
+
+// console.log(len("JavaScript"))
+
+//2 task
+// function join_str(str) {
+//     return str.split(" ").join("")
+// }
+// console.log(join_str("Java Script"))
+
+//3 task
+// function join_str(str) {
+//     return str.split(" ").join("_")
+// }
+// console.log(join_str(" Java Script !"))
+
+//4 task
+// function str4(str) {
+//     return str.slice(0, 3) == "ipt"
+// }
+
+// console.log(str2("Java Script"))
+
+// 5 task
+// function str5(str) {
+//     return str.slice(str.length - 3, str.length) == "ipt"
+// }
+
+// console.log(str3("Java Script"))
+
+//6 task
+// function str6(str) {
+//     return str[0].toUpperCase()
+// }
+
+// console.log(str6("java Script"))
+
+//7 task
+// function str7(str) {
+//     return str[str.length-1].toLowerCase()
+// }
+
+// console.log(str7("java ScripT"))
+
+//8 task
+// let str = ("He2ll0oWor3l4d");
+// str = str.split("");
+// for(i = 0; i<str.length; i++){
+//     if(Number(str[i]) > 0 || Number(str[i]) <= 0){
+//         str.splice(i, 1);
+//     }
+// }
+// console.log(str.join(""));
+
+//9 task
+// let str = ("abcefdwertwsdrwerwryhtrviwervioweugucuywe");
+// str = str.split("");
+// const regex = new RegExp(' ', 'g');
+// let glass = ['a', 'e', 'i', 'u', 'y', 'o']
+// for (i=0; i<glass.length; i++){
+//     for(j=0; j<str.length; j++){
+//         if(str[j] == glass[i]){
+//             str.splice(j, 1);
+//         }
+//     }
+// }
+
+// console.log(str.join(""))
+
+//10 task
+// let str = ("Hello Java Script !")
+// console.log(str.split(" "))
